9d8b12ba7a53d8bd5954c15d3ec67ffd524015b2
mya413 <lsm940413@gmail.com>
2024-04-02 17:19:22 +0900

    feat: sessionStorage set, redirect 경로 설정, enter 키 입력 이벤트에 따른 알람 메시지 설정

--- src/components/molecules/NameInput.tsx
+++ src/components/molecules/NameInput.tsx
@@ -1,28 +1,50 @@
 import styled from "styled-components";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import InputButton from "../atoms/InputButton";
+import { useNavigate } from "react-router-dom";
 
 export default function NameInput() {
+  const navigator = useNavigate();
+
+  const [inputName, setInputName] = useState("");
+
   const [inputCount, setInputCount] = useState(0);
   const MAX_LENGTH = 10;
 
-  const inputCountHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length > MAX_LENGTH) {
       e.target.value = e.target.value.slice(0, MAX_LENGTH);
     }
+    setInputName(e.target.value);
     setInputCount(e.target.value.length);
   };
+
+  const enterKeyHandler = (e: any) => {
+    if ((e.key === "Enter" || e.key.code === 13) && inputName) {
+      e.preventDefault();
+      navigator("/");
+      setInputName(e.target.value);
+      console.log(inputName);
+    } else if ((e.key === "Enter" || e.key.code === 13) && !inputName) {
+      alert("이름 또는 닉네임을 입력해주세요!");
+    }
+  };
+
+  useEffect(() => {
+    window.sessionStorage.setItem("name", JSON.stringify(inputName));
+  }, [inputName]);
 
   return (
     <NameInputStyle>
       <input
         id="nameInput"
         type="text"
-        placeholder="Please enter your name or nickname..."
-        onChange={inputCountHandler}
+        placeholder="당신의 이름 또는 닉네임을 입력해주세요..."
+        onChange={inputHandler}
         maxLength={MAX_LENGTH}
+        onKeyDown={enterKeyHandler}
       />
-      <InputButton />
+      <InputButton inputName={inputName} />
       <label htmlFor="nameInput">{inputCount}/10자</label>
     </NameInputStyle>
   );
@@ -42,22 +64,26 @@ const NameInputStyle = styled.div`
     background-color: #eee;
     outline-color: #86c5ff;
   }
+
   & > button {
     position: absolute;
     right: 5px;
     top: 10px;
+
     &:hover {
       & > svg {
         color: #222;
         transition: all 0.3s;
       }
     }
+
     & > svg {
       font-size: 20px;
       color: #bbb;
       transition: all 0.3s;
     }
   }
+
   & > label {
     position: absolute;
     top: 50px;
