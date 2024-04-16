0afb23a01e19d04ae13d7b81b5e551c594f3028a
mya413 <lsm940413@gmail.com>
2024-04-17 00:37:46 +0900

    feat: svg path id와 일치하는 데이터 id 값에 따른 스타일 수정

--- src/components/atoms/Map.tsx
+++ src/components/atoms/Map.tsx
@@ -1,10 +1,35 @@
 import { ReactComponent as MapImage } from "@/assets/map.svg";
+import { MouseEventHandler } from "react";
 import styled from "styled-components";
+import { location } from "@/utils/location";
 
-export default function Map() {
+interface MapProps {
+  setLocationInfo: any;
+}
+
+export default function Map({ setLocationInfo }: MapProps) {
+  const onClickHandler: MouseEventHandler<SVGSVGElement> = (e) => {
+    const element = e.target as SVGPathElement;
+    const clickedElementId = element.id;
+    const isLocationMatched = location.filter(
+      (loc) => loc.id === clickedElementId
+    );
+
+    if (isLocationMatched && clickedElementId) {
+      const activeElement = document.querySelector(".active");
+
+      element.classList.add("active");
+      setLocationInfo(isLocationMatched);
+
+      if (activeElement) {
+        activeElement.classList.remove("active");
+      }
+    }
+  };
+
   return (
     <MapStyles>
-      <MapImage />
+      <MapImage onClick={onClickHandler} />
     </MapStyles>
   );
 }
