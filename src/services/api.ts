74931dd897dbf43206069ff3f50e83aba4fcef68
mya413 <lsm940413@gmail.com>
2024-04-17 12:14:48 +0900

    refactor: 환경변수 추가 및 코드 수정

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,12 @@ import { hours, minutes } from "@/utils/time";
 
 export default function Api(setFn: any, locationInfo: any) {
   const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = process.env.REACT_APP_API_URL;
   const BASE_DATE = year + month + day;
   const BASE_TIME = hours + String(Number(minutes) - 30);
   const LOCATION = locationInfo;
 
-  const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${API_KEY}&numOfRows=60&pageNo=1&base_date=${BASE_DATE}&base_time=${BASE_TIME}&nx=${LOCATION?.[0]?.nx}&ny=${LOCATION?.[0]?.ny}&dataType=json`;
+  const url = `${API_URL}?serviceKey=${API_KEY}&numOfRows=60&pageNo=1&base_date=${BASE_DATE}&base_time=${BASE_TIME}&nx=${LOCATION?.[0]?.nx}&ny=${LOCATION?.[0]?.ny}&dataType=json`;
 
   fetch(url)
     .then((res) => res.json())
