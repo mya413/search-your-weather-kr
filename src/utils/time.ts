c8b245ae677546ced4a376ba7045ede02eb45600
mya413 <lsm940413@gmail.com>
2024-04-18 21:50:53 +0900

    remove: 불필요한 파일 삭제

--- src/utils/time.ts
+++
@@ -1,5 +0,0 @@
-import { today } from "./date";
-
-export const hours = String(today.getHours()).padStart(2, "0");
-export const minutes = String(today.getMinutes()).padStart(2, "0");
-export const seconds = String(today.getSeconds()).padStart(2, "0");
